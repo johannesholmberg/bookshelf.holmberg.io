71710b9cb1762cbd4520e3979558ced21382b221
Johannes Holmberg <johannes@holmberg.io>
2017-05-20 17:58:12 +0200

    Add base values for critical css

--- tasks/gulp/criticalcss.js
+++ tasks/gulp/criticalcss.js
@@ -1,38 +1,35 @@
-var gulp = require('gulp'),
-    critical = require('critical');
+const gulp = require('gulp'),
+      critical = require('critical');
 
 gulp.task('critical', function() {
 
-  // Run critical for these different page types and generate a minified css file for each
-  var base = '_site/',
-      css = 'source/assets/css/styles.css',
-      width = 1900,
-      height = 1200,
-      minify = false,
-      ignore = ['@font-face'];
+  const base = './',
+        css = '_site/assets/css/styles.css',
+        width = 1400,
+        height = 1100,
+        minify = false,
+        ignore = ['@font-face'];
 
-  // Index
   critical.generate({
     base: base,
-    src: 'index.html',
-    css: [css],
+    src: '_site/index.html',
     dest: 'source/_includes/criticalcss/index.css',
+    css: css,
     width: width,
     height: height,
+    minify: minify,
     ignore: ignore,
-    minify: minify
   });
 
-  // Book
   critical.generate({
     base: base,
-    src: 'farnhams-freehold/index.html',
-    css: [css],
+    src: '_site/the-glass-castle/index.html',
     dest: 'source/_includes/criticalcss/book.css',
+    css: css,
     width: width,
     height: height,
+    minify: minify,
     ignore: ignore,
-    minify: minify
   });
 
 });
