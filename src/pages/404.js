ba6af64bf725f388382e4bdab239dd18617dd33c
Johannes Holmberg <johannes@holmberg.io>
2018-04-18 10:14:04 +0200

    Add styles for 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Link from 'gatsby-link'
 
 const NotFoundPage = () => (
-  <div>
+  <div className="error-404">
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>
+      Go to the <Link to="/">start page</Link>
+    </p>
   </div>
 )
 
