4f5eb5715177e4ada3302a3f68b87d2017b6f33f
Johannes Holmberg <johannes@holmberg.io>
2018-04-23 11:32:05 +0200

    Remove highlighted quote

--- src/components/highlight.js
+++ src/components/highlight.js
@@ -1,14 +1,95 @@
-import React from 'react'
+import React, { Component } from 'react'
+import jump from 'jump.js'
 
-const Highlight = props => {
-  return (
-    <blockquote id={`location-${props.quote.loc}`} className="highlight">
-      <p>{props.quote.quote}</p>
-      <a href={`#location-${props.quote.loc}`} className="highlight__meta">
-        Location {props.quote.loc}
-      </a>
-    </blockquote>
-  )
+import Icon from '../components/icon'
+
+export default class Highlight extends Component {
+  componentDidMount() {
+    if (window.location.hash) {
+      const hash = window.location.hash.replace('#', '')
+      if (document.getElementById(hash)) {
+        this.activateHighlight(hash)
+      }
+    }
+  }
+
+  highlightSwitch(e) {
+    const targetLink = e.currentTarget.href.toString()
+
+    if (targetLink && targetLink.indexOf('#') > -1) {
+      const hash = targetLink.substr(targetLink.indexOf('#') + 1)
+      const highlight = document.getElementById(hash)
+      if (highlight.className.indexOf('highlight--active') === -1) {
+        this.removeHighlight()
+        this.activateHighlight(hash)
+        e.preventDefault()
+        this.updateURL(hash)
+      }
+    }
+  }
+
+  activateHighlight(hash) {
+    const highlight = document.getElementById(hash)
+    highlight.className += ' highlight--active'
+    highlight.focus()
+  }
+
+  updateURL(hash) {
+    const hashIndex = window.location.href.indexOf('#')
+    let hashlessURL
+    if (hashIndex === -1) {
+      hashlessURL = window.location.href
+    } else {
+      hashlessURL = window.location.href.slice(0, hashIndex)
+    }
+
+    jump(`#${hash}`, {
+      duration: 500,
+    })
+
+    history.replaceState(null, null, hashlessURL + '#' + hash)
+  }
+
+  clearHighlight() {
+    this.removeHighlight()
+    const hashIndex = window.location.href.indexOf('#')
+    const hashlessURL = window.location.href.slice(0, hashIndex)
+    history.replaceState(null, null, hashlessURL)
+  }
+
+  removeHighlight() {
+    const highlighted = document.querySelector('.highlight--active')
+    if (highlighted !== null) {
+      highlighted.classList.remove('highlight--active')
+    }
+  }
+
+  render() {
+    const { quote } = this.props
+    return (
+      <blockquote id={`location-${quote.loc}`} className="highlight">
+        <p className="highlight__text">
+          {quote.quote}
+          <button
+            onClick={e => {
+              this.clearHighlight(e)
+            }}
+            className="highlight__clear-button"
+          >
+            <Icon id="close" />
+          </button>
+        </p>
+        <a
+          onClick={e => {
+            this.highlightSwitch(e)
+          }}
+          href={`#location-${quote.loc}`}
+          className="highlight__meta"
+        >
+          <Icon id="link" />
+          Location {quote.loc}
+        </a>
+      </blockquote>
+    )
+  }
 }
-
-export default Highlight
