078a0c53b61865a89b4c8a1ad54413b690e0a590
Johannes Holmberg <johannes@holmberg.io>
2018-05-01 09:23:36 +0200

    Clean up

--- src/components/icon.js
+++ src/components/icon.js
@@ -3,7 +3,12 @@ import PropTypes from 'prop-types'
 
 const Icon = ({ id }) => (
   <span className="svg-wrap">
-    <svg viewBox="0 0 96 96" className="svg-wrap__icon" width="24" height="24">
+    <svg
+      viewBox="0 0 96 96"
+      className="svg-wrap__icon"
+      width="24"
+      height="24"
+    >
       <use xlinkHref={`#${id}`} />
     </svg>
   </span>
